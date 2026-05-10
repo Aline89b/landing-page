338d1e6d7a901a3cf100fefab6d7fb8bf03ba827
Aline89b <a.grianti89@gmail.com>
2026-05-10 20:45:40 +0200

    small fix

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer className="bg-[#b65806] text-white py-4">
             <div className="container mx-auto text-center">
-                <p className="text-sm">© 2025 SlowTravel. Tutti i diritti riservati.</p>
+                <p className="text-sm">© 2026 SloWelcome. Tutti i diritti riservati.</p>
                 <p className="text-sm">Sviluppato da Aline</p>
             </div>
         </footer>
